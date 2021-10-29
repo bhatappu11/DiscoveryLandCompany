6f7635b44f30eb84449354fadcee6a3b77a18ac9
Arpitha Bhat <arpithabht12@gmail.com>
2021-10-29 16:36:53 +0530

    MODIFIED: menu option of header

--- js/header.js
+++ js/header.js
@@ -5,9 +5,9 @@ document.getElementById("header-content").innerHTML =
 </div>
 <div class="nav-menu">
     <div><a href="../pages/about.html">About DLC</a></div>
-    <div><a href="../pages/ourworld.html">Our Worlds</a></div>
+    <div><a href="../pages/ourworld.html"  class="active">Our Worlds</a></div>
     <div><a href="../pages/experiences.html">Experiences</a></div>
-    <div><a href="../pages/gallery.html" class="active">Gallery</a></div>
+    <div><a href="../pages/gallery.html">Gallery</a></div>
     <div><a href="../pages/press.html">Press</a></div>
     <div class="menu-bar" onclick="myFunction(this)">
         <div class="bar1"></div>
