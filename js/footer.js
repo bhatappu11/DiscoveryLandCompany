0bb2ca3f5173b584940b47c07ba6d4e47efaa829
Arpitha Bhat <arpithabht12@gmail.com>
2021-10-29 12:51:17 +0530

    ADDED: created separate js files for header and footer

---
+++ js/footer.js
@@ -0,0 +1,31 @@
+document.getElementById("footer-content").innerHTML =
+`
+<div>
+        <img class="footer-img" src="../assets/logo-crop.png">
+        </div>
+        <div class="right-footer">
+            <div>
+                <P>GET IN TOUCH</P>
+                <P>(480)624-5200</P>
+                <p style="text-decoration: underline;">mail@discoverylandco.com</p>
+            </div>
+            <div>
+                <div><a href="../pages/about.html">About DLC</a></div>
+                <div><a href="../pages/ourworld.html">Our Worlds</a></div>
+                <div><a href="../pages/experiences.html">Experiences</a></div>
+                <div><a href="../pages/gallery.html" class="active">Gallery</a></div>
+                <div><a href="../pages/press.html">Press</a></div>
+            </div>
+            <div>
+                <div><a href="#">Magazine</a></div>
+                <div><a href="#">Careers</a></div>
+                <div><a href="#">Foundation</a></div>
+                <div><a href="#" class="active">Company</a></div>
+                <div><a href="#">Contact</a></div>
+                <div><a href="#">Privacy</a></div>            
+            </div>
+        </div>
+        <div class="copyright-content">
+            &copy; Discovery Land Company.All rights reserved
+</div>
+`;
---
+++ js/header.js
@@ -0,0 +1,18 @@
+document.getElementById("header-content").innerHTML =
+`
+<div class="logo-content">
+<img class="logo-image" src="../assets/logo-discoveryland.png">
+</div>
+<div class="nav-menu">
+    <div><a href="../pages/about.html">About DLC</a></div>
+    <div><a href="../pages/ourworld.html">Our Worlds</a></div>
+    <div><a href="../pages/experiences.html">Experiences</a></div>
+    <div><a href="../pages/gallery.html" class="active">Gallery</a></div>
+    <div><a href="../pages/press.html">Press</a></div>
+    <div class="menu-bar" onclick="myFunction(this)">
+        <div class="bar1"></div>
+        <div class="bar2"></div>
+        <div class="bar3"></div>
+    </div>
+</div>
+`;
