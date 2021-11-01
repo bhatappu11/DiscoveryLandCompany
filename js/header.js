bafa7456cea804f95d34181ee4f5fa0e80bfa24c
Arpitha Bhat <arpithabht12@gmail.com>
2021-11-01 17:42:48 +0530

    [Arpitha]MODIFIED: path to pages and also underline current page in nav menu

--- js/header.js
+++ js/header.js
@@ -1,11 +1,11 @@
 document.getElementById("header-content").innerHTML =
 `
 <div class="logo-content">
-<img class="logo-image" src="../assets/logo-discoveryland.png">
+<img class="logo-image" onclick="window.location='../pages/home.html'" src="../assets/logo-discoveryland.png">
 </div>
 <div class="nav-menu">
-    <div><a href="../pages/about.html">AboutDLC</a></div>
-    <div><a href="../pages/ourworld.html"  class="active">OurWorlds</a></div>
+    <div><a href="../pages/aboutdlc.html">AboutDLC</a></div>
+    <div><a href="../pages/ourworlds.html">OurWorlds</a></div>
     <div><a href="../pages/experiences.html">Experiences</a></div>
     <div><a href="../pages/gallery.html">Gallery</a></div>
     <div><a href="../pages/press.html">Press</a></div>
